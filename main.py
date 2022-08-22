7990e2b0864995d6f466d4650c1315710935e41f
Nathan <salomb3@gmail.com>
2022-08-22 16:09:24 +0100

    update

--- main.py
+++ main.py
@@ -2,65 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
-
-def sneaker_deals():
-    site_url = 'https://www.sneakerdealsgb.com/sneaker-deals/'
-    site_name = get_name_from_url(site_url)
-    page = requests.get(site_url)
-    content = page.content
-    soup = BeautifulSoup(content, "html.parser")
-    today = str(datetime.now().date())
-    title = soup.title.text
-
-    items = soup.findAll('div', {'class': 'grid-item-content'})
-    with open(f"{site_name}.txt", "w", newline="") as f:
-        for item in items:
-            urls = item.find('img')
-            src = urls.get('src')
-            description = item.find('pre').text.strip()
-            description = str(description.encode('utf-8')).replace(r"\xc2\xa3", "£")
-            image_url = f'\nIMAGE URL:\n{src}'
-            full_description = f'\nDESCRIPTION\n{description}'
-
-            f.write(today)
-            f.write('\n')
-            f.write(title)
-            f.write('\n')
-            f.write(image_url)
-            f.write('\n')
-            f.write(full_description)
-
-
-def sneakers_n_stuff():
-    site_url = 'https://www.sneakersnstuff.com/en-gb/56/sale'
-    site_name = get_name_from_url(site_url)
-    page = requests.get(site_url)
-    content = page.content
-    soup = BeautifulSoup(content, "html.parser")
-    today = str(datetime.now().date())
-    title = soup.title.text
-    items = soup.findAll('div', {'class': 'product-list'})
-
-    with open(f"{site_name}.txt", "w", newline="") as f:
-        for item in items:
-            urls = item.find('img')
-            src = urls.get('src')
-            brand = item.find('span', class_='card__brand')
-            name = item.find('strong', class_='card__name')
-            description = brand, ' ', name
-            price_before = item.find('del', class_='price__original')
-            price_after = item.find('span', class_='price__current')
-
-            image_url = f'\nIMAGE URL:\n{src}'
-            full_description = f'\nDESCRIPTION\n{description}\n{price_before}\n{price_after}'
-
-            f.write(today)
-            f.write('\n')
-            f.write(title)
-            f.write('\n')
-            f.write(image_url)
-            f.write('\n')
-            f.write(full_description)
+today = str(datetime.now().date())
 
 
 def get_name_from_url(url):
@@ -68,3 +10,42 @@ def get_name_from_url(url):
     site_url_name = site_url_name[1]
     return site_url_name
 
+
+def deal_scraper(url, item_list_name):
+    site_url = url
+    site_name = get_name_from_url(site_url)
+    page = requests.get(site_url)
+    content = page.content
+    soup = BeautifulSoup(content, "html.parser")
+    title = soup.title.text
+
+    items = soup.findAll('div', {'class': f'{item_list_name}'})
+    with open(f"{site_name}.txt", "w", newline="") as f:
+        f.write(today)
+        f.write('\n')
+        f.write(title)
+        f.write('\n')
+        for item in items:
+            urls = item.find('img')
+            src = urls.get('src')
+            if site_name == 'sneakerdealsgb':
+                description = item.find('pre').text.strip()
+                description = str(description.encode('utf-8')).replace(r"\xc2\xa3", "£")
+            elif site_name == 'sneakersnstuff':
+                brand = item.find('span', class_='card__brand')
+                name = item.find('strong', class_='card__name')
+                price_before = item.find('del', class_='price__original')
+                price_after = item.find('span', class_='price__current')
+                description = brand, ' ', name, '\n', price_before, '\n', price_after
+
+            image_url = f'\nIMAGE URL:\n{src}'
+            full_description = f'\nDESCRIPTION\n{description}'
+
+            f.write(image_url)
+            f.write('\n')
+            f.write(full_description)
+            f.write('\n--------------------------------------\n')
+
+
+deal_scraper('https://www.sneakerdealsgb.com/sneaker-deals/', 'grid-item-content')
+deal_scraper('https://www.sneakersnstuff.com/en-gb/56/sale', 'product-list')
